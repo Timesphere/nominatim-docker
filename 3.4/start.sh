51649112ccf095a3c8c25a29552880c94ab5dc47
Mark <37935198+MarkTimesphere@users.noreply.github.com>
2020-12-14 10:55:42 -0500

    Update start.sh

--- 3.4/start.sh
+++ 3.4/start.sh
@@ -2,9 +2,11 @@
 
 stopServices() {
         service apache2 stop
+        service postgresql stop
 }
 trap stopServices TERM
 
+service postgresql start
 service apache2 start
 
 # fork a process and wait for it
